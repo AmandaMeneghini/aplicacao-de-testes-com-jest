c65718f278a41d92f16ab2705984e6e2480894b2
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-03-18 19:47:50 -0300

    package.json: add watch test for no "npm rum test" all the time

---
+++ calcular-valor-pedidos.test.js
@@ -0,0 +1,4 @@
+it("deve rodar o teste", () => {
+    //assertion ou asserção (o que nós realmente queremos testar)
+    expect(true).toBe(true)
+})
--- sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum');
-
-test('adicionando 1 + 2 é igual a 3', () => {
-  expect(sum(1, 2)).toBe(100);
-});
