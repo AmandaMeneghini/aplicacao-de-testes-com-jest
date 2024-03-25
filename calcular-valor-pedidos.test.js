c1925f74dcbb986e1c2882eb2f25cb6b0dc05c5d
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-03-25 17:34:21 -0300

    calcular-valor-pedido.test.js: add test "... quando o valor for menor que 500"

--- calcular-valor-pedidos.test.js
+++ calcular-valor-pedidos.test.js
@@ -1,9 +1,7 @@
 const calcularValorPedido = require('./calcular-valor-pedidos');
 
 it("não deve cobrar valor de frete quando o valor dos produtos for superior a 500", () => {
-    //AAA
 
-    //ARRANGE
     const meuPedido = {
         itens: [
             {nome: "Arco Encantado", valor: 2000},
@@ -11,13 +9,22 @@ it("não deve cobrar valor de frete quando o valor dos produtos for superior a 5
         ]
     }
 
-    // ACT
     const resultado = calcularValorPedido(meuPedido);
 
     
     expect(resultado).toBe(2000)
 });
 
+it("deve cobrar o valor de frete quando o valor dos produtos for menor que 500", () => {
+    const meuPedido = {
+        itens: [
+            {nome: "Medalhão", valor: 50},
+            {nome: "Entrega", valor: 100, entrega: true}
+        ]
+    }
 
+    const resultado = calcularValorPedido(meuPedido);
 
-
+    
+    expect(resultado).toBe(150)
+});
